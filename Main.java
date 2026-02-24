682d72cac5a4cb3f40bff595cf23aaa6988c9d27
alexlawtengyi6-bit <alexlawtengyi6@gmail.com>
2026-02-24 20:49:29 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.io.Console;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.print("testing for jenkins");
         Console console = System.console();
         
         if (console == null) {
